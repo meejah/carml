99645340fc2e877aafec49e365a320984a36679e
meejah <meejah@meejah.ca>
2015-06-14 00:01:05 -0600

    whitespace

--- setup.py
+++ setup.py
@@ -11,55 +11,46 @@ __url__ = 'https://github.com/meejah/carml'
 __license__ = 'Public Domain (http://unlicense.org/)'
 __copyright__ = 'Copyright 2014'
 
-def pip_to_requirements(s):
-    """
-    Change a PIP-style requirements.txt string into one suitable for setup.py
-    """
-
-    if s.startswith('#') or s.startswith('--'):
-        return ''
-    m = re.match('(.*)([>=]=[.0-9]*).*', s)
-    if m:
-        return '%s (%s)' % (m.group(1), m.group(2))
-    return s.strip()
-
-setup(name = 'carml',
-      version = __version__,
-      description = 'A command-line tool to query and control a running Tor. Based on txtorcon + Twisted.',
-      long_description = open('README.rst', 'r').read(),
-      keywords = ['python', 'twisted', 'tor', 'command-line', 'cli'],
-      install_requires = [line for line in open('requirements.txt').readlines() if not line.startswith('#') and not line.startswith('--')],
-      classifiers = ['Framework :: Twisted',
-                     'Development Status :: 2 - Pre-Alpha',
-                     'Environment :: Console',
-                     'Intended Audience :: Developers',
-                     'License :: Public Domain',
-                     'Natural Language :: English',
-                     'Operating System :: POSIX :: Linux',
-                     'Operating System :: Unix',
-                     'Programming Language :: Python',
-                     'Programming Language :: Python :: 2',
-                     'Programming Language :: Python :: 2.6',
-                     'Programming Language :: Python :: 2.7',
-                     'Topic :: System :: Networking',
-                     'Topic :: Internet :: Proxy Servers',
-                     'Topic :: Internet',
-                     'Topic :: Security',
-                     'Topic :: Utilities'],
-      author = __author__,
-      author_email = __contact__,
-      url = __url__,
-      license = __license__,
-      packages = find_packages(),
-      entry_points={
-          'console_scripts': [
-              'carml = carml.dispatch:dispatch'
-          ]
-      },
-      include_package_data = True,
-      package_data={'': ['*.asc', '*.pem']},
-      data_files=[('share/carml', ['README.rst', 'meejah.asc']),
-                  ('share/carml/doc/', ['doc/' + x for x in filter(lambda x: x.endswith('.rst'), os.listdir('doc'))]),
-                  ('share/carml/example_extension/carml/command', ['example_extension/carml/command/blam.py']),
-              ]
-  )
+setup(
+    name='carml',
+    version=__version__,
+    description='A command-line tool to query and control a running Tor. Based on txtorcon + Twisted.',
+    long_description=open('README.rst', 'r').read(),
+    keywords=['python', 'twisted', 'tor', 'command-line', 'cli'],
+    install_requires=[line for line in open('requirements.txt').readlines() if not line.startswith('#') and not line.startswith('--')],
+    classifiers=[
+        'Framework :: Twisted',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Networking',
+        'Topic :: Internet :: Proxy Servers',
+        'Topic :: Internet',
+        'Topic :: Security',
+        'Topic :: Utilities'],
+    author=__author__,
+    author_email=__contact__,
+    url=__url__,
+    license=__license__,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'carml = carml.dispatch:dispatch'
+        ]
+    },
+    include_package_data=True,
+    package_data={'': ['*.asc', '*.pem']},
+    data_files=[
+        ('share/carml', ['README.rst', 'meejah.asc']),
+        ('share/carml/doc/', ['doc/' + x for x in filter(lambda x: x.endswith('.rst'), os.listdir('doc'))]),
+        ('share/carml/example_extension/carml/command', ['example_extension/carml/command/blam.py']),
+    ]
+)
